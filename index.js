133d434813bb9486af4b53dff00cc53996d087bc
shubhamoverdose <shubham@overdose.digital>
2023-07-04 22:01:31 +0530

    feat: learnt to read and write file asynchronously

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 const fs = require("fs");
 
-// Blocking module
-const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8");
-console.log(textIn);
-const textOut = `this is already written ${textIn} but I am overwriting it at ${Date.now()}`;
-fs.writeFileSync("./starter/txt/output.txt", textOut);
-console.log("file created");
+// Blocking module, synchronous way
+// const textIn = fs.readFileSync("./starter/txt/input.txt", "utf-8");
+// console.log(textIn);
+// const textOut = `this is already written ${textIn} but I am overwriting it at ${Date.now()}`;
+// fs.writeFileSync("./starter/txt/output.txt", textOut);
+// console.log("file created");
 
-// Non-Blocking module
-fs.readFile("./starter/txt/input.txt", "utf-8", (data, err) => {
-  console.log(data);
+// Non-Blocking module , asynchronous way
+fs.readFile("./starter/txt/start.txt", "utf-8", (err, data1) => {
+    fs.readFile(`./starter/txt/${data1}.txt`, "utf-8", (err, data2) => {
+        console.log(data2);
+        fs.readFile(`./starter/txt/append.txt`, "utf-8", (err, data3) => {
+            console.log(data3);
+            fs.writeFile('./starter/txt/final.txt', `I am adding ${data2} data 2 \n and ${data3} data3 `, "utf-8", err =>{
+                console.log('file have been written')
+            })
+          });
+
+
+      });
+
 });
 console.log("reading file");
